8a947e74b0ca1c650084da1e724aab82c79a89ff
nytrist <jacinta@nytrist.com>
2021-03-05 23:24:17 +1100

    baseliner webapp ready

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,6 @@
 		//set correct url to pass to api
 		var thisURL = window.location.protocol + "//" + window.location.host + window.location.pathname
-		var apihost = 'baseliner-japan-api.herokuapp.com'; //local address of api - stations
+		var apihost = '127.0.0.1:5000'; //local address of api - stations
 		var apiURL = window.location.protocol + "//" + apihost + window.location.pathname
 
 		//call api
